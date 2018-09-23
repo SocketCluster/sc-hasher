45f4392586a42805e5bc8b857e911a3b003f341e
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2018-09-23 20:49:54 +0200

    v1.0.1

--- index.js
+++ index.js
@@ -2,20 +2,20 @@
 module.exports.hash = function (key, modulo) {
   var ch;
   var result = 0;
-  if (typeof key == 'number') {
+  if (typeof key === 'number') {
     result = key;
   } else {
     if (key instanceof Array) {
       key = key[0];
     }
-    if (typeof key != 'string') {
+    if (typeof key !== 'string') {
       try {
         key = JSON.stringify(key);
       } catch (e) {
         key = null;
       }
     }
-    if (key == null || key.length == 0) {
+    if (key == null || key.length === 0) {
       return result;
     }
     for (var i = 0; i < key.length; i++) {
